130c67d0e620e1f411e8b4f4ee6808d210c1e8a3
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-11-04 22:28:09 +0100

    Enhanced security middleware configuration with content security policy directives

--- back-end/config/middlewares.ts
+++ back-end/config/middlewares.ts
@@ -1,7 +1,22 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'"],
+          'img-src': [
+            "'self'",
+            'data:',
+            'strapi.io',
+            'cook-and-share.onrender.com'
+          ],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
